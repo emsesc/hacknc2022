1565a251a3e0367de58fc7ea789d231f4728aaff
Andrew Lockard <alockard@unc.edu>
2022-11-05 14:31:25 -0400

    Flask works!

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "GoUNCbeatDOOOOOK"
+
 
 @app.route("/")
+@app.route("/home/")
 def home():
     return render_template("homepage.html")
 
 @app.route("/form/", methods=["POST","GET"])
 def form():
-    if request.method == "post":
-        name = request.form["name"]
-        diet = request.form["diet"]
-        items = request.form["items"]
-        phone = request.form["phone"]
-        return f"<h1>Submission<h1><p>Name: {name}<br>Diet: {diet}<br>Items: {items}<br>Phone: {phone}</p>"
+    if request.method == "POST":
+        session["name"] = request.form["name"]
+        session["diet"] = request.form["diet"]
+        session["food"] = request.form["food"]
+        session["phone"] = request.form["phone"]
+        return redirect(url_for("confirm"))
     else:
         return render_template("index.html")
+
+@app.route("/confirm/")
+def confirm():
+    if "name" in session:
+        data = [session["name"], session["diet"], session["food"], session["phone"]]
+        return render_template("confirm.html", data=data)
+    else:
+        return redirect(url_for("form"))
 
 @app.route("/about/")
 def about():
     return "About page!"
 
-# Redirects
-@app.route("/home/")
-def home_rdr():
-    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
