7baa93933be3c39a1ef00dc749464652e617a550
Andrew Lockard <alockard@unc.edu>
2022-11-05 17:56:21 -0400

    move javascript

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 import requests
+import json
+import re
 
 app = Flask(__name__)
 app.secret_key = "GoUNCbeatDOOOOOK"
@@ -7,15 +9,34 @@ app.secret_key = "GoUNCbeatDOOOOOK"
 def sendToDatabase(json):
     url = "https://lennymenny.azurewebsites.net/api/db-update?code=nx4WB8uAEPnUJvklFRX7r2D4hx_XUvuP44QLJqSWAI20AzFuNgToCg=="
 
-    x = requests.post(url, json = json)
+    x = requests.post(url, data=json)
     print(x.text)
 
-
-def validate_data():
+def convertJSON() -> json:
     if "name" in session:
-        pass
+        data = {
+            "name" : session["name"],
+            "number" : session["phone"],
+            "dietary" : session["diet"],
+            "favorites" : session["food"]
+        }
+        print(json.dumps(data))
+        return json.dumps(data)
     else:
         return redirect(url_for("form"))
+
+def validate_phone():
+    if "phone" in session:
+        p = re.compile('^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$')
+        m = p.match(session["phone"])
+        if m == None:
+            pass # Flash error saying the form is not accepted
+        elif m.group(1) != "1" and m.group(1) != None:
+            pass # Flash error saying we can only send to US numbers
+        else:
+            session["phone"] = "1" + m.group(2) + m.group(3)+ m.group(4)
+    else:
+        redirect(url_for("form"))
 
 @app.route("/")
 @app.route("/home/")
@@ -26,9 +47,11 @@ def home():
 def form():
     if request.method == "POST":
         session["name"] = request.form["name"]
-        print(request.form.getlist("diet"))
+        session["diet"] = request.form.getlist("diet")
         session["food"] = request.form["food"]
         session["phone"] = request.form["phone"]
+        validate_phone()
+        #sendToDatabase(convertJSON())
         return redirect(url_for("confirm"))
 
     else:
@@ -37,7 +60,7 @@ def form():
 @app.route("/confirm/")
 def confirm():
     if "name" in session:
-        data = [session["name"], "PLACEHOLDER", session["food"], session["phone"]]
+        data = [session["name"], session["diet"], session["food"], session["phone"]]
         return render_template("confirm.html", data=data)
     else:
         return redirect(url_for("form"))
