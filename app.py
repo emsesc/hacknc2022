fc2e9fd833180503d01bb72e4eb165a55f805da8
Andrew Lockard <alockard@unc.edu>
2022-11-05 12:05:40 -0400

    Flask go crazy

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("homepage.html")
+
+@app.route("/form/")
+def form():
+    return "Form Page!"
+
+@app.route("/about/")
+def about():
+    return "About page!"
+
+# Redirects
+@app.route("/home/")
+def home_rdr():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
