9c77a87e0daca745349c4955c9ff25fd8e3ad7f9
emsesc <emseschen@gmail.com>
2022-11-05 21:38:50 -0400

    fix

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,71 @@
-from app.py import app
- 
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+import requests
+import json
+import re
+
+app = Flask(__name__)
+app.secret_key = "GoUNCbeatDOOOOOK"
+
+def sendToDatabase(json):
+    url = "https://lennymenny.azurewebsites.net/api/db-update?code=nx4WB8uAEPnUJvklFRX7r2D4hx_XUvuP44QLJqSWAI20AzFuNgToCg=="
+
+    x = requests.post(url, data=json)
+    print(x.text)
+
+
+def convertJSON() -> json:
+    if "name" in session:
+        data = {
+            "name" : session["name"],
+            "number" : session["phone"],
+            "dietary" : session["diet"],
+            "favorites" : session["food"]
+        }
+        print(json.dumps(data))
+        return json.dumps(data)
+    else:
+        return redirect(url_for("form"))
+
+def validate_phone(number) -> str:
+    p = re.compile('^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$')
+    m = p.match(number)
+    if m == None:
+        pass # Flash error saying the form is not accepted
+    elif m.group(1) != "1" and m.group(1) != None:
+        pass # Flash error saying we can only send to US numbers
+    else:
+        return "1" + m.group(2) + m.group(3)+ m.group(4)
+
+@app.route('/postmethod', methods = ["POST"])
+def get_food():
+    jsdata = request.form['javascript_data']
+    session["food"] = json.loads(jsdata)
+    print(session["food"])
+    return session["food"]
+
+@app.route("/", methods=["POST","GET"])
+@app.route("/home/", methods=["POST","GET"])
+@app.route("/form/", methods=["POST","GET"])
+def form():
+    if request.method == "POST":
+        session["name"] = request.form["name"]
+        session["diet"] = request.form.getlist("diet")
+        session["phone"] = request.form["phone"]
+        session["phone"] = validate_phone(session["phone"])
+        sendToDatabase(convertJSON())
+        return redirect(url_for("confirm"))
+
+    else:
+        return render_template("index.html")
+
+@app.route("/confirm/")
+def confirm():
+    if "name" in session:
+        data = [session["name"], session["diet"], session["food"], session["phone"]]
+        return render_template("confirm.html", data=data)
+    else:
+        return redirect(url_for("form"))
+
+
 if __name__ == "__main__":
-        app.run()
+    app.run(debug=True)
